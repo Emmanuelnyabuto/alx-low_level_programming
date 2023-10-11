0366d6009e9dd1add2e4154e62ac05a07270e2bf
EmmanuelNyabuto <emmanuele2ke@gmail.com>
2023-10-11 10:10:30 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
-	/**
-	 * main - print - Entry point
-	 * Return; Always 0
-	 */
 
+/**
+ * main - Entry point
+ * Return; Always 0
+ */
 
-	int main(void)
-	{
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,4 +17,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
